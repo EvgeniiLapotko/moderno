8f914113f2fdaddd24209aeb4e7fde6cc6ed2b5c
Evgenii <lapotko.e@yandex.ru>
2020-11-27 15:45:00 +0300

    new page products details

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,15 @@ $(function () {
         $(".header__box").toggleClass('active');
     });
 
+    $('.product-one__tabs .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
     var mixer = mixitup('.products__inner-box');
 
     $(".products-slider__inner").slick({
@@ -22,6 +31,8 @@ $(function () {
         dots: true,
         arrows: false
     });
+
+
 
 
 
